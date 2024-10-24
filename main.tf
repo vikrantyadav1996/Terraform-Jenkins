53bd821e898a1c73d0d3c34120bc518ab44d4140
vikrantyadav1996 <vikrant.yadav1996@outlook.com>
2024-10-24 15:08:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-    region = "us-east-1"  
+    region = "eu-north-1"  
 }
 
 resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
+  ami           = "ami-08eb150f611ca277f" # us-west-2
   instance_type = "t2.micro"
   tags = {
-      Name = "TF-Instance"
+      Name = "my_instance"
   }
 }
